f47806bd7f66d579a06d9eded3cef44f2dd0de06
valerievozza <valerievozza@gmail.com>
2022-06-30 13:48:12 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const PORT = 8000
+
+const elements = {
+    // objects & properties
+}
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// app.get('endpoint here', (req, res) => {
+    // code
+// })
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is now running on port ${PORT}`)
+})
