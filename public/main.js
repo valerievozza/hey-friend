b9b91c7b75db2da1418b6a4f9c9e84aca9c6816e
valerievozza <valerievozza@gmail.com>
2022-08-04 16:48:01 -0700

    selfcare function working

--- public/main.js
+++ public/main.js
@@ -1,8 +1,10 @@
 const encouragement = document.getElementById("encouragement")
 const cat = document.getElementById("cat")
+const selfCare = document.getElementById("selfCare")
 
 encouragement.addEventListener("click", giveEncouragement)
 cat.addEventListener("click", giveCat)
+selfCare.addEventListener("click", giveSelfCareTip)
 
 function giveEncouragement() {
     const wordsOfEncouragement = [
@@ -11,11 +13,29 @@ function giveEncouragement() {
     "I'm proud of you.",
     "I appreciate you."
 ]
-    const random = Math.floor(Math.random() * max)
+
     const max = wordsOfEncouragement.length - 1
+    const random = Math.floor(Math.random() * max)
 
     console.log(wordsOfEncouragement[random])
-    document.getElementById("response").innerText = wordsOfEncouragement[random]
+    document.getElementById("responseImage").src = ""
+    document.getElementById("responseText").innerText = wordsOfEncouragement[random]
+}
+
+function giveSelfCareTip() {
+    const selfCareTips = [
+    "Take a walk.",
+    "Take a hot shower.",
+    "Eat a vegetable.",
+    "Take 3 long slow deep breaths & exhale slowly."
+]
+
+    const max = selfCareTips.length - 1
+    const random = Math.floor(Math.random() * max)
+
+    console.log(selfCareTips[random])
+    document.getElementById("responseImage").src = ""
+    document.getElementById("responseText").innerText = selfCareTips[random]
 }
 
 function giveCat() {
@@ -24,7 +44,7 @@ function giveCat() {
     .then(res => res.json())
     .then(data => {
         console.log(data[0].url)
-
+    document.getElementById("responseText").innerHTML = ""
     document.getElementById("responseImage").src = data[0].url
     })
     .catch(err => {
