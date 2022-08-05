aca881517b5c75a863fb7a022cb6f5b0dca91a3b
valerievozza <valerievozza@gmail.com>
2022-08-04 21:15:51 -0700

    added save button and localStorage but not currently working

--- public/main.js
+++ public/main.js
@@ -6,6 +6,13 @@ encouragement.addEventListener("click", giveEncouragement)
 cat.addEventListener("click", giveCat)
 selfCare.addEventListener("click", giveSelfCareTip)
 
+// Storage.prototype.setObj = function(key,obj) {
+//     return this.setItem(key, JSON.stringify(obj))
+// }
+// Storage.prototype.getObj = function(key) {
+//     return JSON.parse(this.getItem(key))
+// }
+
 function giveEncouragement() {
     const wordsOfEncouragement = [
     "Good job!",
@@ -20,6 +27,23 @@ function giveEncouragement() {
     console.log(wordsOfEncouragement[random])
     document.getElementById("responseImage").src = ""
     document.getElementById("responseText").innerText = wordsOfEncouragement[random]
+    document.querySelector('#save').classList.toggle('hidden')
+
+    // document.querySelector('#save').addEventListener('click', saveThisItem)
+
+    function saveThisItem() {
+        if (!localStorage.getItem('items')) {
+            localStorage.setItem('items', [])
+        }
+        let existingItems = JSON.parse(localStorage.getItem('items'));
+        existingItems.push(wordsOfEncouragement[random])
+
+        localStorage.setItem('items', JSON.stringify(existingItems))
+    }
+    
+    saveThisItem()
+    console.log(item)
+
 }
 
 function giveSelfCareTip() {
@@ -36,6 +60,7 @@ function giveSelfCareTip() {
     console.log(selfCareTips[random])
     document.getElementById("responseImage").src = ""
     document.getElementById("responseText").innerText = selfCareTips[random]
+    document.querySelector('#save').classList.toggle('hidden')
 }
 
 function giveCat() {
@@ -46,8 +71,24 @@ function giveCat() {
         console.log(data[0].url)
     document.getElementById("responseText").innerHTML = ""
     document.getElementById("responseImage").src = data[0].url
+    document.querySelector('#save').classList.toggle('hidden')
     })
     .catch(err => {
         console.log(`error ${err}`)
     })
+}
+
+//let thisItem = document.getElementById('item').innerHTML
+
+let item = []
+
+if (!localStorage.getItem('items')) {
+    localStorage.setItem('items', item)
+}
+
+// document.querySelector('#save').addEventListener('click', saveThisItem)
+
+function saveThisItem() {
+    item.push('thisItem')
+    localStorage.setItem('items', item)
 }
